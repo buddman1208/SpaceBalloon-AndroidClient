97934c9a443e7b4bce3aed3f0c0c37fb545e8a2c
Junseok Oh <kotohana5706@edcan.kr>
2016-06-15 11:28:04 +0900

    Refactor Directory

--- app/src/main/java/kr/edcan/spaceballoon/activity/MainActivity.java
+++ app/src/main/java/kr/edcan/spaceballoon/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package kr.edcan.spaceballoon;
+package kr.edcan.spaceballoon.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import kr.edcan.spaceballoon.R;
 
 public class MainActivity extends AppCompatActivity {
 
