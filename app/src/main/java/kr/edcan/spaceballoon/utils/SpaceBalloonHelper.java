2df671f8c591b83d0a9c9eaf4c5265ec3cdbc339
Junseok Oh <kotohana5706@edcan.kr>
2016-06-15 11:29:02 +0900

    Implement SpaceBalloonHelper

---
+++ app/src/main/java/kr/edcan/spaceballoon/utils/SpaceBalloonHelper.java
@@ -0,0 +1,15 @@
+package kr.edcan.spaceballoon.utils;
+
+import android.content.Context;
+
+/**
+ * Created by KOHA_CLOUD on 16. 6. 15..
+ */
+public class SpaceBalloonHelper {
+    private Context context;
+
+    public SpaceBalloonHelper(Context context) {
+        this.context = context;
+    }
+
+}
